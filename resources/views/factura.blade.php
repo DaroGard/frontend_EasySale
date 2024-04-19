d365ba6b9b9b330d713944813aa2671df854164b
Alejandro Garcia <AlexDG997@gmail.com>
2024-04-19 17:37:11 -0600

    Arreglado un error CSS

--- resources/views/factura.blade.php
+++ resources/views/factura.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{ url('css/cart.css') }}">
+    <link rel="stylesheet" href="{{ url('css/factura.css') }}">
     <link rel="shortcut icon" href="{{ url('img/Logo.png') }}" type="image/x-icon">
     <title>Factura</title>
 </head>
